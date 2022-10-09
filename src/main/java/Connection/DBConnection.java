683a435b14d2fffc46ab48d5c6109800653235ff
mmateusf <106560647+mmateusf@users.noreply.github.com>
2022-10-09 03:16:06 -0500

    Update DBConnection.java

--- src/main/java/Connection/DBConnection.java
+++ src/main/java/Connection/DBConnection.java
@@ -13,17 +13,18 @@ public class DBConnection {
     //Declaración objeto conector.
     Connection connection;
     //Datos base de datos para url en el driven.
-    static String db = "BikeClinic";
-    static String port = "3306";
+    static String db = "BikeClinic";   // Cambiar "BikeClinic" por "railway"
+    static String port = "3306";       // Cambiar "3306" por "7846"
     static String login = "root";
-    static String pasword = "admin";
+    static String pasword = "admin";   // Cambiar "admin" por "rHaCxK5cvaE8ZSvI17L0"
+    //static String ip = "containers-us-west-92.railway.app";
 
     public DBConnection() {
         //no tiene parametros porque se logea con usuario y contraseña, por lo cual no se realiza. 
         try {
             //registra el driven para la conexión en MySql.
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:" + this.port + "/" + this.db;
+            String url = "jdbc:mysql://localhost:" + this.port + "/" + this.db;     // Cambiar localhost por "+this.ip+"
             connection = DriverManager.getConnection(url, this.login, this.pasword);
             System.out.println("Conexión con la base de datos exitosa.");
         } catch (Exception ex) {
